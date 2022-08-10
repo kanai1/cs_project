5a88f18242b7e545ff87d700e435a07f4e4ff3e0
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-10 15:18:56 +0900

    Create nonmember_buy.php

---
+++ php/nonmember_buy.php
@@ -0,0 +1,23 @@
+<?php
+	$conn = mysqli_connect('localhost', 'rudrleo', 'rudrleoWkd!2', 'CSproject');
+
+	$ticket_number;
+
+	do
+	{
+		$ticket_number = uniqid();
+		$overlap_check = "SELECT * FROM nonmember_ticket WHERE ticket_number = '{$ticket_number}'";
+	}
+	while (mysqli_query($conn, $overlap_check));
+
+	$sql_insert = "INSERT INTO nonmembeR_ticket(ticket_number, created) VALUES('{$ticket_number}', now())";
+
+	if(mysqli_query($conn, $sql_insert))
+	{
+		// 정상 구매
+	}
+	else
+	{
+		// 구매 실패
+	}
+?>
