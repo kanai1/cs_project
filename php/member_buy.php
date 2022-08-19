1fcbb1973289ab0a1edc91facec57f7097e8f23a
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-19 16:05:30 +0900

    Update member_buy.php

--- php/member_buy.php
+++ php/member_buy.php
@@ -10,14 +10,31 @@
 		}
 
 		$conn = mysqli_connect('localhost', 'rudrleo', 'rudrleoWkd!2', 'CSproject');
-		$sql_find = "SELECT * from member_ticket where id = '{$user_id}'";
+		$sql_find = "SELECT ticket_code from user where id = '{$user_id}'";
 
 		if($result = mysqli_fetch_array(mysqli_query($conn, $sql_find)))
 		{
-			$now_count = $result['count'] + $now_count;
-			$sql_alter = "UPDATE member_ticket SET count=$now_count WHERE id='{$user_id}'";
+			$code = $result['ticket_code'];
+			$sql_find_ticket = "SELECT * FROM member_ticket WHERE code='{$code}'";
 
-			if(mysqli_query($conn, $sql_alter))
+			if($result_ticket_code = mysqli_fetch_array(mysqli_query($conn, $sql_find_ticket)))
+			{
+				$now_count = $result['count'] + $now_count;
+				$sql_alter = "UPDATE member_ticket SET count={$now_count} WHERE code='{$code}'";
+
+				if(mysqli_query($conn, $sql_alter))
+				{
+					// 정상 구매
+				}
+				else
+				{
+					// 구매 실패
+				}
+			}
+
+			$sql_insert = "INSERT INTO member_ticket(code,count) VALUES('{$code}', {$count})";
+	
+			if(mysqli_query($conn, $sql_insert))
 			{
 				// 정상 구매
 			}
@@ -28,16 +45,7 @@
 		}
 		else
 		{
-			$sql_insert = "INSERT INTO member_ticket(id,count) VALUES('{$user_id}', $count)";
-
-			if(mysqli_query($conn, $sql_insert))
-			{
-				// 정상 구매
-			}
-			else
-			{
-				// 구매 실패
-			}
+			// 잘못된 접근
 		}
 	}
 	else
