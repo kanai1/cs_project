7e7aa7bd88b6b61fd04215b4e763364fb4c6a9c3
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-20 14:07:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -11,20 +11,23 @@
     <header>
         <a href="index.html" class="title">경기대 식권발매</a>
         <?php
+            $buy_url = "";
             if(isset($_SESSION['user_id']))
             {
                 echo "<a href='mypage.php' class='login'>마이페이지</a>";
+                $buy_url = "member_buy.html";
             }
             else
             {
                 echo "<a href='signin.html' class='login'>로그인</a>";
                 echo "<a href='signup.html' class='register'>회원가입</a>";
+                $buy_url = "nonmember_buy.html";
             }
         ?>
     </header>
     <section>
         <p class="explain">빠르게<br><span class="green">&nbsp맛있게</span></p>
-        <button type="button" onclick="location.href='php/buy.php'" class="buy">식권구매</button>
+        <button type="button" onclick="location.href=<?php echo $buy_url; ?>" class="buy">식권구매</button>
     </section>
     <footer>
         <p class="foot">문의사항</p>
