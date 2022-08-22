1463254c40c82ab0b79a95b929294e0f2c095d17
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-22 23:33:42 +0900

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,39 +1,28 @@
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>마이페이지</title>
-    <link rel="stylesheet" type="text/css" href="mypage.css">
+<?php
+	if(isset($_POST['id']) && isset($_POST['password']))
+	{
+		$id = $_POST['id'];
+		$password = $_POST['password'];
 
-	<?php
-		if(!isset($_SESSION['id']))
+		$conn = mysqli_connect('localhost', 'rudrleo', 'rudrleoWkd!2', 'CSproject');
+		$sql = "SELECT * FROM user WHERE id = '{$id}' AND password = SHA2('{$password}', 256)";
+
+		if($result = mysqli_fetch_array(mysqli_query($conn, $sql)))
 		{
-			echo "<script>alert('로그인 후 이용해주세요.');location.href='/';</script>";
+			session_start();
+
+			$_SESSION['user_name'] = $result['name'];
+			$_SESSION['user_id'] = $result['id'];
+
+			echo "<script>alert('로그인에 성공했습니다.');location.href='/'</script>";
 		}
-		
-		$conn = mysqli_connect('localhost', 'rudrleo', 'rudrleoWkd!2', 'CDproject');
-		$id = $_SESSION['id'];
-		$sql = "SELECT * FROM user WHERE id='{$id}'";
-
-		$result = $mysqli_fetch_array(mysqli_query($conn, $sql));
-	?>
-
-</head>
-<body>
-    <header>
-        <a class="title" href="/">경기대 식권발매</a>
-    </header>
-    
-
-  <div class="main">
-    <p class="sign" align="center"></p>
-    <p class="name">이름: <?php echo $result['name']; ?> </p>
-    <p class="phone">전화번호: <?php echo $result['phone']; ?></p>
-    <p class="email">이메일: <?php echo $result['email']; ?></p>
-    <p class="remainder">남은 식권 갯수:result</p>
-    <p class="code">식권 코드: <?php echo $result['ticket_code']; ?></p>
-    </div>
-</body>
-</html>
+		else
+		{
+			echo "<script>alert('로그인에 실패했습니다.');location.href='../signin.html'</script>";
+		}
+	}
+	else
+	{
+		echo "<script>alert('잘못된 접근입니다.');location.href='signin.html'</script>";
+	}
+?>
