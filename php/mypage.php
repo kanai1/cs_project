7e6c44d197327ad03a19cfb5b33b5b3f8aa42a18
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-22 23:34:04 +0900

    Update mypage.php

--- php/mypage.php
+++ php/mypage.php
@@ -5,7 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>마이페이지</title>
-    <link rel="stylesheet" type="text/css" href="../css/mypage.css">
+    <link rel="stylesheet" type="text/css" href="mypage.css">
+
+	<?php
+		if(!isset($_SESSION['id']))
+		{
+			echo "<script>alert('로그인 후 이용해주세요.');location.href='/';</script>";
+		}
+		
+		$conn = mysqli_connect('localhost', 'rudrleo', 'rudrleoWkd!2', 'CDproject');
+		$id = $_SESSION['id'];
+		$sql = "SELECT * FROM user WHERE id='{$id}'";
+
+		$result = $mysqli_fetch_array(mysqli_query($conn, $sql));
+	?>
 
 </head>
 <body>
@@ -16,11 +29,11 @@
 
   <div class="main">
     <p class="sign" align="center"></p>
-    <p class="name">이름:result </p>
-    <p class="phone">전화번호:result</p>
-    <p class="email">이메일:result</p>
+    <p class="name">이름: <?php echo $result['name']; ?> </p>
+    <p class="phone">전화번호: <?php echo $result['phone']; ?></p>
+    <p class="email">이메일: <?php echo $result['email']; ?></p>
     <p class="remainder">남은 식권 갯수:result</p>
-    <p class="code">남은 식권 코드:relsut</p>
+    <p class="code">식권 코드: <?php echo $result['ticket_code']; ?></p>
     </div>
 </body>
 </html>
