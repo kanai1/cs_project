bf14bf4a0662aaec89ac35f721c53a7a658f0e03
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-23 18:13:13 +0900

    Update mypage.php

--- php/mypage.php
+++ php/mypage.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>마이페이지</title>
-    <link rel="stylesheet" type="text/css" href="mypage.css">
+    <link rel="stylesheet" type="text/css" href="./css/mypage.css">
 
 	<?php
 		if(!isset($_SESSION['user_id']))
