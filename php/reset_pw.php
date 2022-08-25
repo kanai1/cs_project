7a99eab3781dbc66a151a0b9922949d8a9e9bd05
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-25 14:02:10 +0900

    Update reset_pw.php

--- php/reset_pw.php
+++ php/reset_pw.php
@@ -5,8 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>비밀번호 초기화<?php echo $_POST['id']; ?></title>
+    <title>비밀번호 초기화</title>
     <link rel="stylesheet" type="text/css" href="/css/find_pw_find.css">
+    
+    <?php
+        if(!isset($_SESSION['pre_id']))
+		{
+			echo "<script>alert('잘못된 접근입니다.');location.href='../';</script>";
+		}
+    ?>
 </head>
 
 <body>
@@ -19,7 +26,7 @@
                     <label>
                         <!-- <span>PW</span> -->
                         <p style="text-align: left; font-size:12px; color:#666"></p>
-                        <input type="password" name="password" placeholder="비밀번호" class="size" required>
+                        <input type="password" name="password" placeholder="변경할 비밀번호" class="size" required>
                     </label>
                         <!--비밀번호-->
 
