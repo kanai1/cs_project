8f85c8e25712a6937fbfba7529986cb69adc6604
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-25 12:19:19 +0900

    Update use_ticket.php

--- php/use_ticket.php
+++ php/use_ticket.php
@@ -15,12 +15,12 @@
 				// 사용 성공
 				$sql_ticket_time = "INSERT INTO ticket_time(time, ticket_number) VALUES(now(), '{$ticket_number}')";
 				mysqli_query($conn, $sql_ticket_time);
-				echo "비회원 식권 사용 완료";
+				echo "<script>alert('식권사용이 완료되었습니다. 맛있게 드세요.');location.href='../main.php';</script>";
 			}
 			else
 			{
 				// 사용 실패
-				echo "비회원 식권 사용 실패";
+				echo "<script>alert('식권사용에 실패했습니다 잠시후 다시 이용해주세요.');location.href='../main.php';</script>";
 			}
 		}
 		else if($result = mysqli_fetch_array(mysqli_query($conn, $sql_memeber_find)))
@@ -30,7 +30,7 @@
 			if($count <= 0)
 			{
 				// 식권 개수 부족
-				echo "식권 개수 부족";
+				echo "<script>alert('식권의 개수가 부족합니다. 식권을 구매후 이용해주세요.');location.href='../main.php';</script>";
 			}
 			else
 			{
@@ -43,24 +43,24 @@
 					$sql_ticket_time = "INSERT INTO ticket_time(time, ticket_number) VALUES(now(), '{$ticket_number}')";
 					mysqli_query($conn, $sql_ticket_time);
 
-					echo "회원 식권 사용 완료";
+					echo "<script>alert('식권사용이 완료되었습니다. 맛있게 드세요.');location.href='../main.php';</script>";
 				}
 				else
 				{
 					// 사용 실패
-					echo "회원 식권 사용 실패";
+					echo "<script>alert('식권사용에 실패했습니다 잠시후 다시 이용해주세요.');location.href='../main.php';</script>";
 				}
 			}
 		}
 		else
 		{
 			// 잘못된 식권
-			echo "잘못된 식권입니다.";
+			echo "<script>alert('잘못된 식권입니다.');location.href='../main.php';</script>";
 		}
 	}
 	else
 	{
 		// 잘못된 접근
-		echo "잘못된 접근입니다.";
+		echo "<script>alert('잘못된 접근입니다.');location.href='../main.php';</script>";
 	}
 ?>
