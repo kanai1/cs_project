117f8f63ae5ae6d7fffff4ab826a7cf90125a31e
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-25 12:49:22 +0900

    Update main.php

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>구매하기</title>
+    <title>식권사용</title>
     <link rel="stylesheet" type="text/css" href="css/buy.css">
 	<?php
 		$conn = mysqli_connect('localhost', 'rudrleo', 'rudrleoWkd!2', 'CSproject');
