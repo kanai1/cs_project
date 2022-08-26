1d1cff9ce6020246a61f3a93f9d80f8aaa047144
kanai <34500707+kanai1@users.noreply.github.com>
2022-08-26 14:39:00 +0900

    Update register.php

--- php/register.php
+++ php/register.php
@@ -23,27 +23,27 @@
 		if(mysqli_query($conn, $check_overlap) == "")
 		{
 			// 이미 회원정보가 있는 아이디나 휴대전화, 이메일입니다.
-			echo "already exist";
+			echo "<script>alert('이미 회원정보가 존재합니다.');location.href='../signup.html';</script>";
 		}
 		else if(strcmp($pw, $confirm))
 		{
 			// 비밀번호와 비밀번호 확인 불일치
-			echo "password inconsistency";
+			echo "<script>alert('비밀번호가 다릅니다.');location.href='../signup.html';</script>";
 		}
 		else if(mysqli_query($conn, $sql))
 		{
 			// 계정생성 성공
-			echo "success";
+			echo "<script>alert('회원가입이 완료되었습니다.');location.href='../';</script>";
 		}
 		else
 		{
 			// 계정생성 실패
-			echo "fail";
+			echo "<script>alert('회원가입에 실패하였습니다. 잠시후 다시 이용해주세요.');location.href='../signup.html';</script>";
 		}
 	}
 	else
 	{
 		// 잘못된 접근
-		echo "wrong connection";
+		echo "<script>alert('잘못된 접근입니다.');location.href='../';</script>";
 	}
 ?>
